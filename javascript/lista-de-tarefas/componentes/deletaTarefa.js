4d2416a67a0bcd3961ca4daed63f181e6f851121
Danilo Araujo <dnlysid@gmail.com>
2022-02-04 19:19:54 -0300

    adicionando botão remover

---
+++ javascript/lista-de-tarefas/componentes/deletaTarefa.js
@@ -0,0 +1,19 @@
+const BotaoDeleta = () => {
+    const deletaBotao = document.createElement('button')
+    
+    deletaBotao.classList.add('delete-button')
+    deletaBotao.innerText = 'deletar'
+
+    deletaBotao.addEventListener('click', deletarTarefa)
+
+    return deletaBotao
+}
+
+const deletarTarefa = (evento) => {
+    const deletaBotao = evento.target
+    const tarefaCompleta = deletaBotao.parentElement
+
+    tarefaCompleta.remove()
+
+    return deletaBotao
+}
--- javascript/lista-de-tarefas/listaDeTarefas.js
+++ javascript/lista-de-tarefas/listaDeTarefas.js
@@ -47,27 +47,5 @@
         tarefaCompleta.classList.toggle('done')
     }
 
-//
-
-    const BotaoDeleta = () => {
-        const deletaBotao = document.createElement('button')
-        
-        deletaBotao.classList.add('delete-button')
-        deletaBotao.innerText = 'deletar'
-
-        deletaBotao.addEventListener('click', deletarTarefa)
-
-        return deletaBotao
-    }
-
-    const deletarTarefa = (evento) => {
-        const deletaBotao = evento.target
-        const tarefaCompleta = deletaBotao.parentElement
-
-        tarefaCompleta.remove()
-
-        return deletaBotao
-    }
-
 }) ()
 
