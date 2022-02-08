c9f203de92c10452c7d6bc4b9ab20e6dbd173caf
Danilo Araujo <dnlysid@gmail.com>
2022-02-07 21:02:30 -0300

    finalizando onclick dos botões

--- javascript/projeto-midi/scripts/main.js
+++ javascript/projeto-midi/scripts/main.js
@@ -5,9 +5,9 @@ function tocaSom(idElementoAudio) {
 }
 
 for (let i = 0; i < listaDeTeclas.length; i++) {
+    
     const tecla = listaDeTeclas[i];
     const instrumento = tecla.classList[1];
-
     const idAudio = `#som_${instrumento}`
 
     tecla.onclick = () => {
