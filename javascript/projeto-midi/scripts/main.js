8788fcf4cd06ff9a4a7913e83038f14f7216a752
Danilo Araujo <dnlysid@gmail.com>
2022-02-07 21:01:56 -0300

    finalizando onclick dos botões

--- javascript/projeto-midi/scripts/main.js
+++ javascript/projeto-midi/scripts/main.js
@@ -4,10 +4,8 @@ function tocaSom(idElementoAudio) {
     document.querySelector(idElementoAudio).play();
 }
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
-    const tecla = listaDeTeclas[contador];
+for (let i = 0; i < listaDeTeclas.length; i++) {
+    const tecla = listaDeTeclas[i];
     const instrumento = tecla.classList[1];
 
     const idAudio = `#som_${instrumento}`
@@ -16,5 +14,5 @@ while (contador < listaDeTeclas.length) {
         tocaSom(idAudio)
     };
 
-    contador++;
+    i++;
 }
