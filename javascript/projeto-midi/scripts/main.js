3a4a8838a9e319fc723edd1a51be04c4480d1ab9
Danilo Araujo <dnlysid@gmail.com>
2022-02-08 13:14:34 -0300

    finalizando projeto

--- javascript/projeto-midi/scripts/main.js
+++ javascript/projeto-midi/scripts/main.js
@@ -1,7 +1,13 @@
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if (elemento != null && elemento.localName === 'audio') {
+        elemento.play();
+    } else {
+        console.log('Elemento não encontrado ou seletor inválido');
+    }
 }
 
 for (let i = 0; i < listaDeTeclas.length; i++) {
